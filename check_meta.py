9f1f78c8277b0b27322331c72c5cc93fb235697a
lllllyccc <57832413+lllllyccc@users.noreply.github.com>
2026-07-13 20:49:39 +0800

    chore: cleanup temp files

--- check_meta.py
+++
@@ -1,5 +0,0 @@
-import json
-d = json.load(open("/tmp/meta.json"))
-print("vnicId:", d.get("vnicId","?"))
-print("compartmentId:", d.get("compartmentId","?"))
-print("subnetId:", d.get("subnetId","?"))
